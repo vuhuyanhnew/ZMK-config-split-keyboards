ce6b642aece2f4dddc5bc8e949d5e31329f74ab5
kien242tran <kien242tran@gmail.com>
2022-07-18 10:42:41 +0700

    cap nhat

--- config/boards/arm/mytom_v1/board.cmake
+++ config/boards/arm/mytom_v1/board.cmake
@@ -1,9 +1,6 @@
-#
-# Copyright (c) 2021 Polarity Works
+# Copyright (c) 2021 The ZMK Contributors
 # SPDX-License-Identifier: MIT
-#
 
 board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
-
+include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
-#include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
