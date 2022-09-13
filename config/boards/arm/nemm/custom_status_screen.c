455130a3984a5e1171800e5f21072cf6750e5760
kien242 <44479300+kien242@users.noreply.github.com>
2022-09-13 09:28:36 +0700

    test 11

--- config/boards/arm/nemm/custom_status_screen.c
+++ config/boards/arm/nemm/custom_status_screen.c
@@ -5,8 +5,10 @@
  */
 
 #include <zmk/display/widgets/output_status.h>
+#include <zmk/display/widgets/peripheral_status.h>
 #include <zmk/display/widgets/battery_status.h>
 #include <zmk/display/widgets/layer_status.h>
+#include <zmk/display/widgets/wpm_status.h>
 #include <zmk/display/widgets/luna_status.h>
 #include <zmk/display/status_screen.h>
 
@@ -21,8 +23,16 @@ static struct zmk_widget_battery_status battery_status_widget;
 static struct zmk_widget_output_status output_status_widget;
 #endif
 
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_PERIPHERAL_STATUS)
+static struct zmk_widget_peripheral_status peripheral_status_widget;
+#endif
+
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_LAYER_STATUS)
 static struct zmk_widget_layer_status layer_status_widget;
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
+static struct zmk_widget_wpm_status wpm_status_widget;
 #endif
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_LUNA_STATUS)
@@ -36,18 +46,35 @@ lv_obj_t *zmk_display_status_screen() {
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_BATTERY_STATUS)
     zmk_widget_battery_status_init(&battery_status_widget, screen);
-    lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), NULL, LV_ALIGN_IN_TOP_RIGHT, 0, 0);
+    lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), NULL, LV_ALIGN_IN_BOTTOM_RIGHT,
+                 0, 0);
 #endif
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_OUTPUT_STATUS)
     zmk_widget_output_status_init(&output_status_widget, screen);
-    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), NULL, LV_ALIGN_IN_TOP_LEFT, 0, 0);
+    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), NULL, LV_ALIGN_IN_TOP_LEFT, 0,
+                 0);
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_PERIPHERAL_STATUS)
+    zmk_widget_peripheral_status_init(&peripheral_status_widget, screen);
+    lv_obj_align(zmk_widget_peripheral_status_obj(&peripheral_status_widget), NULL,
+                 LV_ALIGN_IN_TOP_LEFT, 0, 0);
 #endif
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_LAYER_STATUS)
     zmk_widget_layer_status_init(&layer_status_widget, screen);
-    lv_obj_set_style_local_text_font(zmk_widget_layer_status_obj(&layer_status_widget),LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, lv_theme_get_font_small());
-    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), NULL, LV_ALIGN_IN_BOTTOM_LEFT, 0, 0);
+    lv_obj_set_style_local_text_font(zmk_widget_layer_status_obj(&layer_status_widget),
+                                     LV_LABEL_PART_MAIN, LV_STATE_DEFAULT,
+                                     lv_theme_get_font_small());
+    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), NULL, LV_ALIGN_IN_BOTTOM_LEFT,
+                 0, 0);
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
+    zmk_widget_wpm_status_init(&wpm_status_widget, screen);
+    lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), NULL, LV_ALIGN_IN_BOTTOM_RIGHT, -12,
+                 0);
 #endif
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_LUNA_STATUS)
@@ -55,4 +82,4 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_align(zmk_widget_luna_status_obj(&luna_status_widget), NULL, LV_ALIGN_CENTER, 0, 0);
 #endif
     return screen;
-}
+}2
